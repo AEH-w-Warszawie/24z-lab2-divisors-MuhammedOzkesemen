22695a8969235a66d902e5c96e4197bc9c7ff3a7
MuhammedOzkesemen <ozkemu_aehit@students.vizja.pl>
2024-10-23 17:29:08 +0200

    Faster Algorythm

--- enc_temp_folder/bd620524b2c118b82b94ce67626f9e2/DivisorsLib.cpp
+++ enc_temp_folder/bd620524b2c118b82b94ce67626f9e2/DivisorsLib.cpp
@@ -6,7 +6,6 @@ std::vector<unsigned long long> divisors(unsigned long long number)
 	std::vector<unsigned long long> result;
 	for (unsigned long long i = 1; i <= number; ++i)
 	{
-		// faster alg
 		if (number % i == 0)
 			result.push_back(i);
 	}
